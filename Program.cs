e10cc86267bb4e7b1e28dc6604daf16db35666e3
mrmdshahriar <shahriar.ict@gmail.com>
2026-09-13 18:09:33 +0600

    fgfgf

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using WebApiForJWT.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<JwtService>();
 
 var app = builder.Build();
 

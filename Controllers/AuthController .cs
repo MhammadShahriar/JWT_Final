fe4a42ee414b39ed3e8e283ffabce9a6903953ba
mrmdshahriar <shahriar.ict@gmail.com>
2026-09-14 10:30:58 +0600

    dfdfd

---
+++ Controllers/AuthController .cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiForJWT.Services;
+
+namespace WebApiForJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtService _jwtService;
+
+        public AuthController(JwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "1234")
+            {
+                var token = _jwtService.GenerateToken(
+                    1,
+                    "admin",
+                    "Admin");
+
+                return Ok(new
+                {
+                    token = token
+                });
+            }
+
+            return Unauthorized(new
+            {
+                message = "Invalid username or password"
+            });
+        }
+    }
+}
+

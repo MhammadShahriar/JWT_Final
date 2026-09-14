ba2dfef0fe160a6e1a95548d4dd2b06931cbece7
mrmdshahriar <shahriar.ict@gmail.com>
2026-09-14 10:07:19 +0600

    dfdffd

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
-
 
 
 builder.Services.AddScoped<JwtService>();
@@ -43,6 +41,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             NameClaimType = ClaimTypes.Name
         };
     });
+
+
 
 builder.Services.AddAuthorization();
 
